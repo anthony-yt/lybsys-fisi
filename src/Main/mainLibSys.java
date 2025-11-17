fc9597606e842a9b29d3ddbf2f98a73015d3ebb2
GaelPapu <gaelcarbajo@gmail.com>
2025-11-17 15:57:44 -0500

    Main en espaniol

--- src/Main/mainLibSys.java
+++ src/Main/mainLibSys.java
@@ -1,11 +1,11 @@
 package Main;
 
-import Home.*;
+import Inicio.*;
 
 public class mainLibSys {
 
     public static void main(String[] args) {
-        Sidebar vMainHome = new Sidebar();
+        barraLateral vMainHome = new barraLateral();
         vMainHome.setVisible(true);
     }
     
